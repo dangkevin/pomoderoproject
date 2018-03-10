9d7bd4299b182c69e9d13139ed7902d86a3ddc95
Kevin Dang <kevin@Kevins-Mac-mini.local>
2018-03-09 18:37:07 -0800

    Added seconds countdown

--- test.js
+++ test.js
@@ -1,5 +1,29 @@
+
+var globalMin;
+var globalSec;
+
 function start(){
-  countdown();
+  var x = document.getElementById("timer").innerHTML;
+  var array = x.split(":");
+  var array0 = array[0];
+  var array1 = array[1];
+  globalMin = parseInt(array0, 10);
+  globalSec = parseInt(array1, 10);
+  var counter = setInterval(timer,1000);
+}
+
+function timer(){
+  if(globalSec == 00 && globalMin > 0){
+    globalSec = 59;
+    console.log("did i get in here");
+  }
+  console.log(globalSec);
+  if(globalSec == 00 && globalMin == 0){
+    clearInterval(counter);
+    return;
+  }
+  document.getElementById("timer").innerHTML="25:" + globalSec;
+  globalSec--;
 }
 
 function reset(){
@@ -7,11 +31,8 @@ function reset(){
 }
 
 
-function countdown(){
-  var x = document.getElementById("timer").innerHTML;
-  var array = x.split(":");
-  console.log(array[0]);
-  console.log(array[1]);
 
+
+function countdown(){
 
 }
