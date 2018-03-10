9ba6895e54df9a3570c707c1d58aa9a8bc7ab68f
Kevin Dang <kevin@Kevins-Mac-mini.local>
2018-03-09 19:15:07 -0800

    more pomo

--- test.js
+++ test.js
@@ -1,6 +1,7 @@
 
 var globalMin;
 var globalSec;
+var counter;
 
 function start(){
   var x = document.getElementById("timer").innerHTML;
@@ -9,30 +10,27 @@ function start(){
   var array1 = array[1];
   globalMin = parseInt(array0, 10);
   globalSec = parseInt(array1, 10);
-  var counter = setInterval(timer,1000);
+  counter = setInterval(timer,1000);
 }
 
 function timer(){
+
   if(globalSec == 00 && globalMin > 0){
     globalSec = 59;
+    globalMin--;
     console.log("did i get in here");
   }
   console.log(globalSec);
   if(globalSec == 00 && globalMin == 0){
     clearInterval(counter);
+    console.log("did i clear it?");
     return;
   }
-  document.getElementById("timer").innerHTML="25:" + globalSec;
+  if(globalSec )
+  document.getElementById("timer").innerHTML= globalMin + ":" + globalSec;
   globalSec--;
 }
 
 function reset(){
   document.getElementById("timer").innerHTML = "00:00";
-}
-
-
-
-
-function countdown(){
-
 }
