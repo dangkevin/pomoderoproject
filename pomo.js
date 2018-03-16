cded821473324a0a0847519d77b038f4d3460216
Kevin Dang <kevin@kevins-mac-mini.edwardstech.net>
2018-03-15 20:51:16 -0700

    udpate

--- pomo.js
+++ pomo.js
@@ -10,27 +10,41 @@ function start(){
   var array1 = array[1];
   globalMin = parseInt(array0, 10);
   globalSec = parseInt(array1, 10);
-  counter = setInterval(timer,1000);
+  console.log("This is the global min before" + " " + globalMin);
+  console.log("This is the global sec before" + " " + globalSec);
+  counter = setInterval(timer, 1000);
 }
 
 function timer(){
 
-  if(globalSec == 00 && globalMin > 0){
-    globalSec = 59;
+console.log(globalSec);
+
+if(globalSec == 0 && globalMin > 0){
     globalMin--;
+    globalSec = 59;
     console.log("did i get in here");
   }
+
+  if(globalSec < 10){
+    globalSec = ("0" + globalSec).slice(-2);
+    console.log("The value of the second is less than 9" + " " + globalSec);
+  }
+
   console.log(globalSec);
-  if(globalSec == 00 && globalMin == 0){
+
+  if(globalSec == 0 && globalMin == 0){
     clearInterval(counter);
     console.log("did i clear it?");
     return;
   }
-  if(globalSec )
+
+
   document.getElementById("timer").innerHTML= globalMin + ":" + globalSec;
+  console.log(globalSec);
   globalSec--;
 }
 
+//function
 function reset(){
   document.getElementById("timer").innerHTML = "00:00";
 }
